48cbd20f70bad7bee2abde2c74744d6d9e61c09f
Timmy Douglas <git@timmyd.info>
2012-04-05 22:32:44 -0400

    nearby

---
+++ snaptinerary.com/place-info.pl
@@ -0,0 +1,101 @@
+#!/usr/bin/perl -wT
+
+use strict;
+use CGI;
+use DBI;
+
+require "./include.pl";
+
+
+my $q = CGI->new;
+my $dbh = db_connect();
+my ($sid, $uid, $displayname, $status) = check_session($q, $dbh);
+my $lid = $q->param('lid');
+my $displaywebsite = '';
+my @tags = get_tags($dbh, $lid);
+
+
+print_start($q, "Snaptinerary Place Information");
+print_top($uid);
+
+my $sth = $dbh->prepare("SELECT lat,long,name,address,type,price,phone,website,description FROM locations WHERE lid = ?");
+$sth->execute($lid);
+
+if (my @row = $sth->fetchrow_array()) {
+    my ($lat,$long,$name,$address,$type,$price,$phone,$website,$description) = @row;
+
+    print "<script type='text/javascript'
+      src='http://maps.googleapis.com/maps/api/js?key=AIzaSyDDILpxDDz6m3lfL6eAtocIPH90B2LXpFU&sensor=false'></script>";
+    print "<script type='text/javascript'>//<![CDATA[
+\$(document).ready(function() {
+var myOptions = {
+  center: new google.maps.LatLng($lat, $long),
+  zoom: 8,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+var map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
+});
+
+//]]></script>
+";
+
+    print "<div class='maincontent'>";
+    print "<h1>$name (" . ('$' x $price) . ")</h1>";
+    print "<h2>$phone</h2>";
+    $displaywebsite = $website;
+
+    if ($website !~ /^http/) {
+        $website = 'http://' . $website;
+    }
+    $displaywebsite =~ s{http[s]?://}{}g;
+
+    print "<h2><a href='$website'>$displaywebsite</a></h2>";
+#AIzaSyDDILpxDDz6m3lfL6eAtocIPH90B2LXpFU
+    print "<h3><a href='http://maps.google.com/q=$address'>$address</a></h2>";
+    print "<div>Tags: ";
+    while (my $idx = shift @tags) {
+        my $tid = shift @tags;
+        my $tag = shift @tags;
+        #
+        print "$tag ";        
+    }
+    print "</div>";
+
+    if ($description ne '') {
+#    print "<div style='background-color: #e0e0e0;'>$description</div>";
+    print "<div>$description</div>";
+    }
+ 
+
+    print "<div id='map_canvas' style='width: 640; height: 480'></div>";
+
+    print "</div>";
+
+} else {
+print "<div class='maincontent'>";
+print "<h1>Cannot find location</h1>";
+print "</div>";
+}
+
+
+
+$sth->finish;
+
+
+print_footer($uid, $status);
+
+print $q->end_html;
+
+
+$dbh->rollback;
+$dbh->disconnect;
+
+
+
+
+
+
+
+
+
+
